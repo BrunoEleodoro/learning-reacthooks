21ea934e6fb15089598acd50ad41e2f16ae51aeb
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2020-04-11 20:13:24 +0000

    baseweb

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from "./pages/home/Home"
 
 function App() {
-    const [nome, setNome] = useState('bruno');
+    // const [nome, setNome] = useState('bruno');
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    {nome}
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-        </a>
-                <button onClick={() =>setNome('Carlos')} >Clique</button>
-            </header>
+        // <div className="App">
+        //     <header className="App-header">
+        //         <img src={logo} className="App-logo" alt="logo" />
+        //         <p>
+        //             {nome}
+        //         </p>
+        //         <a
+        //             className="App-link"
+        //             href="https://reactjs.org"
+        //             target="_blank"
+        //             rel="noopener noreferrer"
+        //         >
+        //             Learn React
+        // </a>
+        //         <button onClick={() =>setNome('Carlos')} >Clique</button>
+        //     </header>
+        // </div>
+        <div>
+            {Home()}
         </div>
     );
 }
